339cf20d4f076f358abc2d2a39968aff0eddbd0a
BColeWebDev <briancole1994@hotmail.com>
2023-08-21 10:24:29 -0400

    wip:home page guards

--- client/src/shared/guards/auth.guards.ts
+++ client/src/shared/guards/auth.guards.ts
@@ -1,24 +1,24 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Subscription, Observable } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from '@angular/router';
+// @Injectable({ providedIn: 'root' })
+// export class AuthGuard implements CanActivate {
+//   constructor(private router: Router, private authService: AuthService) {}
 
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { AuthService } from 'src/app/auth/auth.service';
+//   canActivate(): Observable<boolean> {
+//     return this.authService.getUser().pipe(
+//       map(user => {
+//         if (user !== null) {
+//           return true;
+//         }
 
-
-@Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanActivate {
-  constructor(private router: Router, private authService: AuthService) {}
-
-  canActivate(): Observable<boolean> {
-    return this.authService.getUser().pipe(
-      map(user => {
-        if (user !== null) {
-          return true;
-        }
-
-        this.router.navigateByUrl('/auth/login');
-        return false;
-      })
-    );
-  }
+//         this.router.navigateByUrl('/auth/login');
+//         return false;
+//       })
+//     );
+//   }
