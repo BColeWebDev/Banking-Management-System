7454796521396fd04f2ac069c64c28e24519c274
BColeWebDev <Briancole1994@hotmail.com>
2023-08-21 15:28:48 -0400

    auth guards

---
+++ client/src/app/home/home.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/shared/shared.module';
+@NgModule({
+    imports: [SharedModule,],
+    declarations: [],
+
+})
+export class HomeModule{}
---
+++ client/src/app/home/home.routing.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes:Routes =[
+
+]
+
+
+export const HomeRoutingModule = RouterModule.forChild(routes);
